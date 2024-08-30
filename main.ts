fcf20c7ee9a3ac6cbb69cea100c0d82c0794d1c1
n8dgr8 <nathan@kwintek.net>
2024-08-29 20:10:58 -0500

    More good stuff

--- main.ts
+++ main.ts
@@ -3,17 +3,53 @@ import { upgradeWebSocket } from "https://deno.land/x/hono@v4.3.11/adapter/deno/
 
 const app = new Hono();
 
-app.parkingLot = {
-    'spot1': 'occupied',
-    'spot2': 'occupied',
-    'spot3': 'unoccupied',
-    'spot4': 'occupied'
-};
+const kv = await Deno.openKv();
+
+type Spot = 'spot1' | 'spot2' | 'spot3' | 'spot4';
+
+type SpotStatus = 'occupied' | 'unoccupied';
 
 
+app.put('/spot/:id', async (c) => {
+  const spotId: number = parseInt(c.req.param('id'), 10);
 
-app.get('/', (c) => {
-  return(c.json(app.parkingLot));
+  if( (spotId <= 4) && (spotId >= 1) ) {
+    const spotKey = 'spot' + spotId;
+    const requestBody = await c.req.json();
+
+    const spotStatus: SpotStatus = requestBody.status as SpotStatus;
+
+    await kv.set(
+      [
+        '300-apollo',
+        'parking-lot',
+        spotKey
+      ],
+      spotStatus
+    );
+    return c.text('OK', 201);
+  }
+  else {
+    return c.text('Not OK', 401);
+  }
+});
+
+app.get('/', async (c) => {
+  const spots = kv.list({prefix: ['300-apollo', 'parking-lot']});
+
+  const returnStructure = {};
+
+  for await (const spot of spots) {
+    const spotKey: string = spot.key.at(-1) as string;
+    returnStructure[spotKey] = spot.value;
+  }
+
+  return(
+    c.json(
+      returnStructure,
+      200
+    )
+  );
 });
 
 Deno.serve(app.fetch);
