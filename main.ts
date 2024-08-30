2a1b1c2fb88d32f87e1e1599ad700d34369712b3
n8dgr8 <nathan@kwintek.net>
2024-08-29 21:24:47 -0500

    Storing WS handles like its 2009

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { Hono } from "https://deno.land/x/hono@v4.3.11/mod.ts";
 import { upgradeWebSocket } from "https://deno.land/x/hono@v4.3.11/adapter/deno/index.ts";
+import { WSContext, type WSReadyState } from "https://deno.land/x/hono@v4.3.11/helper/websocket/index.ts";
 
 const app = new Hono();
 
@@ -9,6 +10,23 @@ type Spot = 'spot1' | 'spot2' | 'spot3' | 'spot4';
 
 type SpotStatus = 'occupied' | 'unoccupied';
 
+const websockets: WSContext[] = [];
+
+app.get(
+  '/ws',
+  upgradeWebSocket(() => {
+    return {
+      async onOpen(_event, ws: WSContext) {
+        websockets.push(ws);
+        ws.send(JSON.stringify(await getParkingLot()));
+      },
+      onClose: (_event, ws: WSContext) => {
+        const wsIndex = websockets.indexOf(ws);
+        websockets.splice(wsIndex, 1);
+      }
+    }
+  })
+)
 
 app.put('/spot/:id', async (c) => {
   const spotId: number = parseInt(c.req.param('id'), 10);
@@ -27,6 +45,15 @@ app.put('/spot/:id', async (c) => {
       ],
       spotStatus
     );
+
+    const parkingLot = JSON.stringify(await getParkingLot());
+
+    for (const websocket of websockets) {
+      if (websocket.readyState == 1) {
+        websocket.send(parkingLot);
+      }
+    }
+
     return c.text('OK', 201);
   }
   else {
@@ -34,16 +61,21 @@ app.put('/spot/:id', async (c) => {
   }
 });
 
-app.get('/', async (c) => {
+const getParkingLot = async () => {
   const spots = kv.list({prefix: ['300-apollo', 'parking-lot']});
 
-  const returnStructure = {};
+  const parkingLot = Object();
 
   for await (const spot of spots) {
     const spotKey: string = spot.key.at(-1) as string;
-    returnStructure[spotKey] = spot.value;
+    parkingLot[spotKey] = spot.value;
   }
 
+  return parkingLot;
+}
+
+app.get('/', async (c) => {
+  const returnStructure = await getParkingLot();
   return(
     c.json(
       returnStructure,
